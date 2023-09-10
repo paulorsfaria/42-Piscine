bc9a4aec36307ade2372e0f4ec9cf7b4852659b2
Paulo Ricardo Dos Santos Faria <paulo-do@c1r3s2.42porto.com>
2023-09-10 18:20:46 +0100

    hahaha you look like a stick

--- C05/ex07/ft_find_next_prime.c
+++ C05/ex07/ft_find_next_prime.c
@@ -6,8 +6,42 @@
 /*   By: paulo-do <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/10 18:09:18 by paulo-do          #+#    #+#             */
-/*   Updated: 2023/09/10 18:09:22 by paulo-do         ###   ########.fr       */
+/*   Updated: 2023/09/10 18:20:15 by paulo-do         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+int ft_is_prime(int nb)
+{
+	int	i;
 
+	i = 2;
+	if (nb <= 1)
+		return(0);
+	while(i <= nb / 2)
+	{
+		if(nb % i == 0)
+		{
+			return(0);
+		}
+		i++;
+	}
+	return(1);
+}
+
+int ft_find_next_prime(int nb)
+{
+	int	primos;
+
+	primos = ft_is_prime(nb);
+
+	if (primos == 1)
+		return (nb);
+	else
+	{
+		while (primos == 0)
+		{
+			primos = ft_is_prime(nb + 1);
+		}
+		return (nb);
+	}
+}
