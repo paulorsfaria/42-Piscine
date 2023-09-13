d4ff6539216552b90999368db7e0b06cac5f406b
Paulo Ricardo Dos Santos Faria <paulo-do@c1r8s8.42porto.com>
2023-09-13 15:25:02 +0100

    ?Nobody expects the Spanish Inquisition

--- C04/ex02/ft_putnbr.c
+++ C04/ex02/ft_putnbr.c
@@ -33,12 +33,30 @@ void	ft_putnbr(int nb)
 	{
 		ft_putnbr(nb / 10);
 		nb = nb % 10; 
-	}
+	} 
 	if (nb < 10)
 		ft_putchar(nb + 48);
 }
-/*
+
 int	main(void)
 {
-	ft_putnbr(-2147483648);
-}*/
+	ft_putnbr(83648);
+}
+/*
+void	ft_putnbr(int nb)
+{
+	long n = nb;
+
+	if (nb < 0)
+	{
+		ft_putchar('-');
+		nb = -nb;
+	}
+	if (nb >= 10)
+	{
+		ft_putnbr(nb / 10); 
+	} 
+
+	ft_putchar((nb % 10) + 48);
+}
+*/
