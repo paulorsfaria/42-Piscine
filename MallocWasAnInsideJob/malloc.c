8baaa5feaa579a11806d49eb4a485d2619b2524d
Paulo Ricardo Dos Santos Faria <paulo-do@c1r8s6.42porto.com>
2023-09-14 15:06:35 +0100

    “One day, lad, all this will be yours.” “What, the curtains?”

--- MallocWasAnInsideJob/malloc.c
+++ MallocWasAnInsideJob/malloc.c
@@ -9,5 +9,38 @@
 /*   Updated: 2023/09/14 13:14:30 by paulo-do         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
+#include <stdlib.h>
+#include <stdio.h>
 
+char *ft_strdup(char *src)
+{
+	int	len;
+	int	i;
+	char	*newStr;
+	
+	i = 0;
+	len = 0;
+	
+	while(src[len] != '\0')
+		len++;
 
+	newStr = (char*)malloc(sizeof(char) * (len + 1));
+	
+	if(newStr == NULL)
+		return(NULL);
+		
+	while(i < len)
+	{
+		newStr[i] = src[i];
+		i++;
+	}
+	
+	return(newStr);
+}
+int	main(int argc, char **argv)
+{
+	if(argc < 2)
+		return(0);
+	printf("%s", ft_strdup(argv[1]));
+	return (0);
+}
